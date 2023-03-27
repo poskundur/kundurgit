df35d8c5eedcb95df0a9a45fc1bb707dc40b5985
poskundur <129078003+poskundur@users.noreply.github.com>
2023-03-27 21:41:33 +0700

    Create Main.java

---
+++ JAVA/src/Main.java
@@ -0,0 +1,12 @@
+class Sman {
+    String nama;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Sman siswa = new Sman();
+        siswa.nama = "hakim";
+
+        System.out.println(siswa.nama);
+    }
+}
